fc4748cb9dcb21ebd1c4f420a4602cc509438916
Ved Shivhare <vedtherockerz@gmail.com>
2026-03-04 19:02:17 +0530

    "Full Pipeline Done !"

--- test.py
+++
@@ -1,6 +0,0 @@
-from src.pipelines.data_pipeline import DataPipeline
-
-if __name__ == "__main__":
-    pipeline = DataPipeline()
-    pipeline.run_pipeline()
-    print("Data pipeline executed successfully.")
